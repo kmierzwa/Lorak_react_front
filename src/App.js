38a8fd8000a70026f2633d64ac6c22cf3d4d53aa
Developers <cepielm@gmail.com>
2021-06-16 20:27:30 +0000

    API

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React, { useState, useEffect } from 'react';
 import Contacts from './components/contacts';
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import configData from "./config.json";
 
 function App() {
   const [users, setUsers] = useState();
-  
- 
+
+
+
   useEffect(async () => {
     //axios jest do feczowania
     const result = await axios(
-      'https://api.lorak.com.pl',
+      configData.SERVER_URL,
     );
     setUsers(result.data);
-  });
+  },[]);
 
   return (
     <div>
--- src/components/AddUserButton.js
+++ src/components/AddUserButton.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 import axios from 'axios'
+import configData from "../config.json";
 
 const AddUserButton = ({ zdupydomordyparametr, UserFirstName, lastName }) => {
 // w lastname nie trzeba robic xx:xx bo paramter jest taki sam jak zmienna
   const sendRequest = () => {
-    const result = axios.post('http://localhost:8081/dupa', { firstName:UserFirstName, lastName, login: 'd', pass: 'd'});
+    const result = axios.post(configData.SERVER_URL+'dupa', { firstName:UserFirstName, lastName, login: 'd', pass: 'd'});
   }
   return (
     <Button onClick={()=>sendRequest()}>
