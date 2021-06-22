2d021d1febc97d01e6fc1696e3614192f1fe7a0f
rzegorz Stuczynski <stuczynski.g@gmail.com>
2021-06-22 09:47:51 +0200

    fixed remove users checkboxes and button

--- src/components/DataTables/Users.js
+++ src/components/DataTables/Users.js
@@ -1,28 +1,46 @@
 import React, { useState, useEffect } from "react";
 import Table from "react-bootstrap/Table";
-import {Container,Row,Col} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import AddButton from "../AddButton";
-import RemoveButton from "../RemoveButton"
+import RemoveButton from "../RemoveButton";
 import Header from "../Header";
 import axios from "axios";
 import configData from "../../config.json";
-import '../../App.css';
-
+import "../../App.css";
 
 const GetUsers = () => {
   const [users, setUsers] = useState();
+  const [selectedUsersIds, setSelectedUsersIds] = useState([]);
+
   useEffect(async () => {
-    //dobrze byloby obsłuzyć błąd serwera - i napisać coś userowi - dałem loading - ale tak naprawde powinny być 3 stany: ok, loading, błąd
-    const result = await axios(configData.SERVER_URL +'/showusers');
+    debugger;
+    const result = await axios(configData.SERVER_URL + "/showusers");
     setUsers(result.data);
-  }, users);
-  let ids =['dupa','dupaa']
-  const testlog = () => {
-    for( var i = 0; i < ids.length; i++){ 
-      console.log(ids[i])
-      }
-  }
-  //onsubmit dodaje zeby przeladowac komponent
+    //ta tablica na dole ma zawierac komponenty które jeżeli się zmienią - ten useEffect jest wołany -
+    // dla treningu spróbuj teraz usunąć usera - poprostu się usunie, a jak dodasz users do tablicy
+    //to sfetchuje się na nowo. teraz macie znowu decyzje architektury  - opcje są takie
+    // 1 wysyłasz idiki na backend i tylko usuwasz ze state userów - raczej zły pomysł bo jak coś pójdzie nie tak to na backendzie zostaną userzy - chyba że wyświetlisz bład...
+    // 2. backend morze odrazu zwracać wszystkich userów i tylko aktualizujesz state
+    // 3. Wymuszasz wywołanie useEffect
+    // Kolejną sprawą jest jak to będzie usuwane - czy przygotujesz api na jedno id i wtedy requesty w pętli
+    // czy 1 request z tablicą
+  }, []);
+
+  const handeleRemoveUsersButton = () => {
+    //tutaj aktualizuję state tak tylko żeby wam pokazać, ale docelowo tu ma iść request na backend (checkbox zostaje zaznaczony - nie chce mi się z tym bawić bo docelowo i tak userzy będą fetchowani na nowo chyba)
+    setUsers(() =>
+      users.filter((user) => !selectedUsersIds.includes(user.id_user))
+    );
+  };
+
+  const handleSelectUserCheckbox = (isChecked, userId) => {
+    isChecked
+      ? setSelectedUsersIds((prev) => [...prev, userId])
+      : setSelectedUsersIds(
+          selectedUsersIds.filter((selectedUser) => selectedUser !== userId)
+        );
+  };
+
   return (
     <>
       <Header />
@@ -30,7 +48,7 @@ const GetUsers = () => {
         <Row className="add-space">
           <Col>
             <AddButton onSubmit={setUsers} />
-            <RemoveButton onSubmit={testlog}/>{" "}
+            <RemoveButton onSubmit={handeleRemoveUsersButton} />
           </Col>
         </Row>
       </Container>
@@ -55,21 +73,13 @@ const GetUsers = () => {
                         <input
                           type="checkbox"
                           id="flexCheckDefault"
-                          onChange={(e) => {
-                            if (e.target.checked === true){
-                              ids.push(user.id_user)
-                              console.log(ids.length)
+                          onChange={(e) =>
+                            handleSelectUserCheckbox(
+                              e.target.checked,
+                              user.id_user
+                            )
                           }
-                            else{
-                              console.log("usuwam")
-                              for( var i = 0; i < ids.length; i++){ 
-                                if ( ids[i] === user.id_user) { 
-                                    ids.splice(i, 1); 
-                                }
-                            
-                            }
-                          }}
-                          }></input>
+                        ></input>
                       </td>
                       <td>{user.id_user}</td>
                       <td>{user.firstname_user}</td>
--- src/components/RemoveButton.js
+++ src/components/RemoveButton.js
@@ -1,25 +1,24 @@
-import {Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import axios from "axios";
 import configData from "../config.json";
 
-const RemoveButton = () => {
-
-  const handleSubmit = (e) => {
-    var ids
-    console.log('dupa')
-    e.preventDefault();
-    axios
-      .post(`${configData.SERVER_URL}removeuser`, {
-        ids,
-      })
-      .catch((e) =>
-        console.log(e)
-      );
-  };
+const RemoveButton = ({ onSubmit }) => {
+  // const handleSubmit = (e) => {
+  //   var ids
+  //   console.log('dupa')
+  //   e.preventDefault();
+  //   axios
+  //     .post(`${configData.SERVER_URL}removeuser`, {
+  //       ids,
+  //     })
+  //     .catch((e) =>
+  //       console.log(e)
+  //     );
+  // };
 
   return (
     <>
-      <Button variant="danger" onClick={handleSubmit}>
+      <Button variant="danger" onClick={onSubmit}>
         Delete user
       </Button>
     </>
